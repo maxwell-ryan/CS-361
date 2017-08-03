7bea3e9a02e0a379720345bf21b6952f1d115396
maxwell-ryan <weinbema@oregonstate.edu>
2017-08-02 18:45:26 -0600

    Update node_server.js
    Adding boilerplate setup code to link database.

--- node_server.js
+++ node_server.js
@@ -1,1 +1,25 @@
+var express = require('express');
+var app = express();
 
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+var request = require('request');
+var mysql = require('mysql');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 8666);
+
+var pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'mysql.eecs.oregonstate.edu',
+  user: 'cs340_',
+  password: '',
+  database: 'cs340_',
+  dateStrings: 'date'
+});
